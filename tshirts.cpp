393e2a181ed60c3f41c6f68c80607c22963803d8
FRANCIS95144 <71829125+FRANCIS95144@users.noreply.github.com>
2025-08-26 15:44:38 +0530

    Update tshirts.cpp

--- tshirts.cpp
+++ tshirts.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <assert.h>
-
+ 
 char size(int cms) {
     char sizeName = '\0';
     if(cms < 38) {
@@ -12,11 +12,14 @@ char size(int cms) {
     }
     return sizeName;
 }
-
+ 
 void testTshirtSize() {
     std::cout << "\nTshirt size test\n";
     assert(size(37) == 'S');
     assert(size(40) == 'M');
     assert(size(43) == 'L');
+    // Strengthen: test the missing boundaries
+    assert(size(38) == 'S'); // Should fail (returns '\0')
+    assert(size(42) == 'L'); // Should fail (returns '\0')
     std::cout << "All is well (maybe!)\n";
 }
